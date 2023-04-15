48ff0f2eff7c457105d57133bc06a7175eefe464
aihamnedal <94713844+aihamnedal@users.noreply.github.com>
2023-04-15 14:26:21 -0500

    provided some API endpoints

--- mainv2.py
+++ mainv2.py
@@ -6,6 +6,28 @@ url: str = config.SUPABASE_URL
 key: str = config.SUPABASE_KEY
 supabase: Client = create_client(url, key)
 
-res = supabase.table('Courses').select('*').execute()
+"""
+The fetch functions return a list of dictionaries which contain row values
+"""
+def fetch_schools() -> list:
+    res = supabase.table('Schools').select('*').execute()
+    return list(res.data)
 
-print(res)
+def fetch_majors(school: int) -> list:
+    res = supabase.table('Curriculum').select('*').eq('school', school).eq('type', "major").execute()
+    return list(res.data)
+
+def fetch_minors(school: int) -> list:
+    res = supabase.table('Curriculum').select('*').eq('school', school).eq('type', "minor").execute()
+    return list(res.data)
+
+def fetch_unique_courses(school: int) -> list:
+    res = supabase.table('Courses').select('*').eq('school', school).execute()
+    return list(res.data)
+
+# Retrieve course from course id (found in curriculum for example)
+def get_course(school: int, id: int) -> dict:
+    data = fetch_unique_courses(school)
+    for course in data:
+        if course['id'] == id:
+            return course
